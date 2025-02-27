50b93e960194cd8b62ae259bbf8666a0bb201859
Kimberly Sioco <kimberly040105@gmail.com>
2025-02-27 23:45:07 +0800

    Set up admin panels

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import Recipe, RecipeIngredient, Ingredient
 
-# Register your models here.
+class TaskInline(admin.TabularInline):
+    model = RecipeIngredient
+    
+class TaskGroupAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [TaskInline, ]
+
+class TaskAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+    search_fields = ('ingredient', 'recipe')
+    list_display = ('ingredient', 'quantity', 'recipe')
+    list_filter = ('recipe', 'ingredient')
+
+admin.site.register(Recipe, TaskGroupAdmin)
+admin.site.register(RecipeIngredient, TaskAdmin)
