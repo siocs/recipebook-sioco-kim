2a710213fa973ff4142145bbb12ea05fd4b3a251
Kimberly Sioco <kimberly040105@gmail.com>
2025-02-27 23:58:53 +0800

    Edited pep8

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,35 +1,38 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ledger:ingredient-detail', args=[str(self.pk)])
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ledger:recipe-detail', args=[str(self.pk)])
+
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=255)
     ingredient = models.ForeignKey(
         Ingredient,
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
         null=False,
         related_name='ingredients'
     )
     recipe = models.ForeignKey(
         Recipe,
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
         null=False,
         related_name='recipe'
     )
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Recipe, Ingredient, RecipeIngredient
 
+
 def recipes_list(request):
     recipes = Recipe.objects.all()
     ctx = {
@@ -8,10 +9,12 @@ def recipes_list(request):
     }
     return render(request, 'ledger/recipes_list.html', ctx)
 
+
 def recipe_detail(request, pk):
     ctx = {
         "recipe": Recipe.objects.get(pk=pk),
-        "ingredients" : RecipeIngredient.objects.filter(recipe=Recipe.objects.get(pk=pk)),
-        
+        "ingredients":
+        RecipeIngredient.objects.filter(recipe=Recipe.objects.get(pk=pk)),
+
     }
     return render(request, 'ledger/recipe_detail.html', ctx)
