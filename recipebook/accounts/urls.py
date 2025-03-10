200b4abde9d12c3df9930463badb79241a8c6b98
Kimberly Sioco <kimberly040105@gmail.com>
2025-03-11 00:21:50 +0800

    Cleaned up code

--- recipebook/accounts/urls.py
+++ recipebook/accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import custom_login, custom_logout
+from .views import custom_login
 
-urlpatterns = [
-    path('login', custom_login, name='login'),
-    path('logout', custom_logout, name='logout')
-]
+urlpatterns = [path('login', custom_login, name='login')]
 
 app_name = "accounts"
--- recipebook/accounts/views.py
+++ recipebook/accounts/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def custom_login(request):
     return render(request, 'registration/login.html')
-
-
-def custom_logout(request):
-    return render(request, 'registration/logout.html')
